f8fb7251cce2e8d75705422642473fcbe548ddf5
ShiningSpring <harutoto1101@gmail.com>
2024-07-12 00:13:20 +0900

    reset

--- 個人/家庭教師時代/javaプロジェクト/的当的遊戯/src/的当的遊戯/AppStarter.java
+++
@@ -1,8 +0,0 @@
-package 的当的遊戯;
-
-public class AppStarter{
-	public static void main(String[] args) {
-		//初期化時はタイトルを表示する
-		AppManager.start(ViewType.TITLE);
-	}
-}
--- 個人/家庭教師時代/javaプロジェクト/的当的遊戯/src/的当的遊戯/ViewType.java
+++
@@ -1,5 +0,0 @@
-package 的当的遊戯;
-
-public enum ViewType {
-	GAME,TITLE,RANKING;
-}
